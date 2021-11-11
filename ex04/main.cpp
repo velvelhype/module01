0dffa596666d6e4ca12db8246aeb80c9584aa799
Your Name <kamori@admin-PC.localdomain>
2021-11-11 23:24:43 +0900

    ex04

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -5,7 +5,6 @@
 
 int main(int argc, char **argv)
 {
-	// if ((ret = args_checker(fileName, oldStr, newStr, fileStream)))		// return (ret);
     if(argc != 4)
     {
         return 1;
@@ -16,24 +15,24 @@ int main(int argc, char **argv)
 	tempStream << fileStream.rdbuf();
 	std::string		fileContent = tempStream.str();
 
-	// if (fileContent.empty())
-		// return (error_manager(5, "source file is empty, or you selected a directory"));
+	if (fileContent.empty())
+		return 0;
 	std::string			oldStr(argv[2]);
 	size_t				oldLen = oldStr.length();
 	std::string			newStr(argv[3]);
 	size_t				newLen = newStr.length();
-	
     size_t				pos = 0;
 	while ((pos = fileContent.find(oldStr, pos)) != std::string::npos)
 	{
-		fileContent.replace(pos, oldLen, newStr);
+		fileContent.erase(pos, oldLen);
+		fileContent.insert(pos, newStr);
 		pos += newLen;
 	}
 	std::string	newFileName = fileName + ".replace";
 	std::ofstream		newFileStream;
 	newFileStream.open(newFileName.c_str());
-	// if (!(newFileStream.good()))
-		// return(error_manager(4, "a problem occured while accessing the .replace file."));
+	if (!(newFileStream.good()))
+		return 1;
 	newFileStream << fileContent;
 	return (0);
 }
